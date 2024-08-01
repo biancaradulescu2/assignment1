71fa500371ac842a3ae4cfd9a26ad595c86a8385
biancaradulescu2 <bianca.radulescu@publicissapient.com>
2024-08-01 15:26:06 +0300

    Try

---
+++ docker_images_test.sh
@@ -0,0 +1,11 @@
+START_VERSION=$(( $1 + 1 ))
+
+
+for (( i=START_VERSION; i<=15; i++ )); do
+    VERSION_TAG="v$i"
+    echo "Tagging and pushing version: $VERSION_TAG"
+    docker tag test biancaradulescu/assignment1_test:$VERSION_TAG
+    docker push biancaradulescu/assignment1_test:$VERSION_TAG
+done
+
+echo "Done"
