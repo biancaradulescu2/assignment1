e64c8edc28c42c70c36abebb27297acf9d2b13d0
biancaradulescu2 <bianca.radulescu@publicissapient.com>
2024-08-01 14:51:40 +0300

    Changes withou deploy

---
+++ commit_number.sh
@@ -0,0 +1,11 @@
+# Read the current version from version.txt
+version=$(cat version.txt)
+echo "Current version: $version"
+
+# Increment the version number
+IFS='.' read -r -a version_parts <<< "$version"
+((version_parts[2]++))
+new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
+echo "New version: $new_version"
+
+echo $new_version > version.txt
